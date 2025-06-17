ea7773463113e52427e3e56a80863b8d93f159e1
V-Fries <vincentfries@proton.me>
2025-06-17 18:19:17 +0800

    doc: Fixed the link to ApiCreator in JsonRpc

--- lib/json_rpc.ex
+++ lib/json_rpc.ex
@@ -2,7 +2,7 @@ defmodule JsonRpc do
   @moduledoc """
   JSON-RPC 2.0 implementation in Elixir.
 
-  Read the JsonRpc.ApiCreator documentation to get started.
+  Read the `JsonRpc.ApiCreator` documentation to get started.
 
   See [EthereumApi](https://github.com/ZenHive/ethereum-api-elixir/) for a complete example.
   """
