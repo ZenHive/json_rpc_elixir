5a1360faef29f294731da5433cd52c22bff0e107
V-Fries <vincentfries@proton.me>
2025-09-01 20:32:58 +0800

    feat: Exported formatter rules

--- .formatter.exs
+++ .formatter.exs
@@ -1,17 +1,22 @@
+locals_without_parens = [
+  method: 1,
+  doc: 1,
+  response_type: 1,
+  parsing_error_type: 1,
+  response_parser: 1,
+  args: 1,
+  args_transformer!: 1,
+  timeout: 1,
+  retries: 1,
+  retry_on_timeout?: 1,
+  time_between_retries: 1
+]
+
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [
-    method: 1,
-    doc: 1,
-    response_type: 1,
-    parsing_error_type: 1,
-    response_parser: 1,
-    args: 1,
-    args_transformer!: 1,
-    timeout: 1,
-    retries: 1,
-    retry_on_timeout?: 1,
-    time_between_retries: 1
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
   ]
 ]
